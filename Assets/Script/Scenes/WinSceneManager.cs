ecf7e498a6abc2d4ad3df2377dd427de8e7ed28a
gameboy1642 <parsnip618@mju.ac.kr>
2025-08-04 08:57:05 +0900

    2025-08-04 update

--- Assets/Script/Scenes/WinSceneManager.cs
+++ Assets/Script/Scenes/WinSceneManager.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine;
 
 public class WinSceneManager : MonoBehaviour
 {
